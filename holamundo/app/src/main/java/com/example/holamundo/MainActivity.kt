53be5b8fc3e4dfa34dc599994a37df3b7537fd6b
Gabriel Biroli <gabrielbiroli@gmail.com>
2024-10-28 14:57:44 +0100

    Clase Android

---
+++ holamundo/app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.holamundo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    //Primer metodo del ciclo de vida: Juntar logica (ths) cn lo grafico (layout)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        // Inicializo las cosas graficas
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
